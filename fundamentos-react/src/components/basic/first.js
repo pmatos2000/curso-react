55f684c25220e296e5a7d2ba6b76a8e3603b0f93
pmatos2000 <pmatos2000@gmail.com>
2020-11-08 16:49:20 -0300

    Primeiro componente II

--- fundamentos-react/src/components/basic/first.js
+++ fundamentos-react/src/components/basic/first.js
@@ -1,3 +1,12 @@
-const First = () => "Primeiro Componente";
+const First = () => {
+    const msg = "Seja bem vindo(a)!";
+
+    return(
+        <div>
+            <h2>Primeiro componente!</h2>
+            <p>{ msg }</p>
+        </div>
+    );
+};
 
 export default First;
