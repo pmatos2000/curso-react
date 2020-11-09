933969d3ebbf6e72e55fa849905f8859126ef4d9
pmatos2000 <pmatos2000@gmail.com>
2020-11-09 20:39:26 -0300

    Fragmentos

---
+++ fundamentos-react/src/components/basic/fragment.js
@@ -0,0 +1,11 @@
+const Fragment = () => {
+    return(
+        <>
+            <h2>Fragmentado</h2>
+            <h3>Fragmentei...</h3>
+            <h4>Frag...</h4>
+        </>
+    );
+}
+
+export default Fragment;
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,12 +3,14 @@ import React from "react"; //Para usar o JSX
 import "./index.css";
 import First from "./components/basic/first";
 import Second from "./components/basic/second";
+import Fragment from "./components/basic/fragment"
 
 ReactDOM.render(
   <div>
       <First />
       <Second title="Situação do aluno" student="Pedro" score="8" />
       <Second title="Situação do aluno" student="Lucas" score="3" />
+      <Fragment />
   </div>,
   document.getElementById("root")
 ); 
